f05cf4d7ee5c8daf45d8f5ebcdf7a7a301699b14
Steve Kolock <sakolock@gmail.com>
2017-05-06 13:07:32 -0400

    Created comments, basic user roles

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,41 @@
+# Create a new category model-backed form
+#  - with validations.
+# You'll need to:
+#  - add a route
+#  - create controller/actions 
+#  - create view templates.
+
+class CategoriesController < ApplicationController
+  before_action :set_category, only: [:show, :edit, :update]
+  before_action :require_user, only: [:new, :create]
+
+  def index
+    @categories = Category.all
+  end
+
+  def show; end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "#{@category.name} successfully created."
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ActiveRecord::Base
+  belongs_to :creator, foreign_key: 'user_id', class_name: 'User'
   belongs_to :post
-  belongs_to :user
+
+  validates :body, presence: true
 end
